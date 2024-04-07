fb9e8789dbe95a19d8d5831590732f15d5f6bf9e
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-07 21:19:33 +0530

    Update primitiveData.java

--- FirstJavaProject/primitiveData.java
+++ FirstJavaProject/primitiveData.java
@@ -1,37 +1,45 @@
 package com.company;
 public class primitveData {
   public static void main(String[] args){
-    byte Variable1 = 8;
+    byte age = 18;
+    System.out.println(age);
     // range -128 to 127 [ -(2^8)/2 to (2^8)/2-1 ] 
     // 1 byte
     
-    short Variable2 = 129;
+    short age1 = 18;
+    System.out.println(age1);
     // range -(2^16)/2 to (2^16)/2-1 
     // 2 byte
 
-    int Variable3;
+    int age2 = 18;
     // range -(2^32)/2 to (2^32)/2-1 
     // 4 byte
     
-    float Variable4;
+    float f1 = 18.4f;
     // range depends 
     // 4 byte
     // default value 0.0f
 
-    long Variable5;
+    long ageCloudy = 9999999999L;
     // range -(2^64)/2 to (2^64)/2-1 
     // 8 byte
 
-    double Variable6;
+    double d1 = 18.88D;
     // range depends
     // 8 byte
     // default value 0.0d
 
-    char Variable7;
+    char ch = 'A';
     // range 0 to 65535(2^16-1)
     // 2 byte
     // supports unicode
     // Default value \u0000
+
+    boolean a = true;
+    // size depends on JVM
+
+    String str = "CLOUDYORK";
+    System.out.println(str);
 
     int num1 = 5;
     int num2 = 5;
