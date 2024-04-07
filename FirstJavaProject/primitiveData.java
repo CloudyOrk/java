8494bb0f1bd8426372ae66037bac2f1a06064196
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-07 21:09:00 +0530

    Update primitiveData.java

--- FirstJavaProject/primitiveData.java
+++ FirstJavaProject/primitiveData.java
@@ -1,6 +1,6 @@
 package com.company;
 public class primitveData {
-  public static void primitive(String[] args){
+  public static void main(String[] args){
     byte Variable1 = 8;
     // range -128 to 127 [ -(2^8)/2 to (2^8)/2-1 ] 
     // 1 byte
