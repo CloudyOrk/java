b3205ff4a375c570fcdd427586702d81d627b974
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-08 18:31:19 +0530

    Create problem2.java

---
+++ FirstJavaProject/src/com.cloudy/problem2.java
@@ -0,0 +1,40 @@
+package com.cloudy;
+import java.util.Scanner;
+public class problem2{
+  public static void main(String[] args){
+    // 1
+    float math = 7/4.0f * 9/2.0f; // first 7/4 then multiple with 9 then divide by 2
+    System.out.println(math);
+
+    // 2 
+    char grade = "A";
+    grade = (char)(grade = 8);
+    System.out.println(grade);
+    gradedecrypt = (char)(grade - 8);
+    System.out.println(gradedecrypt);
+
+    // 3
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your age")
+    int age = sc.nextInt();
+    System.out.println(age<18; "you are not allowed!"; "you are allowed!");
+
+    // 4
+    int v = 10;
+    int u = 3;
+    int a = 7;
+    int s = 36;  
+    float exp = 9v*v.0f - u*u.0f)/(2as.0f);
+    System.out.println(exp);
+
+    // 5
+    int x = 7;
+    int a = 7.49/7.0f + 35/7.0f;
+    System.out.println(a);
+  }
+}
+
+    
+    
+    
+      
