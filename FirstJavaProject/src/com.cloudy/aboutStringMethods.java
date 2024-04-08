07a94f26baa20f61d2d725230c0cdf080c97f87e
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-08 21:25:13 +0530

    Update aboutStringMethods.java

--- FirstJavaProject/src/com.cloudy/aboutStringMethods.java
+++ FirstJavaProject/src/com.cloudy/aboutStringMethods.java
@@ -41,31 +41,52 @@ public class aboutStringMethods{
 
     // string startswith
     String name7 = "ClOuDyporK";
-    String startswithStr1 = name6.startswith("Cl");  // returns true
+    String startswithStr1 = name7.startswith("Cl");  // returns true
     System.out.println(startswithStr1);
-    String startswithStr2 = name6.startswith("ork"); // returns false
+    String startswithStr2 = name7.startswith("ork"); // returns false
     System.out.println(startswithStr2);
 
     // string charAt(int)
     String name8 = "ClOuDyporK";
-    String charAtStr1 = name6.charAt(3);  // returns u
+    String charAtStr1 = name8.charAt(3);  // returns u
     System.out.println(charAtStr1);
-    String charAtStr2 = name6.charAt(0); // returns c
+    String charAtStr2 = name8.charAt(0); // returns c
     System.out.println(charAtStr2);
 
     // string endswith
     String name9 = "ClOuDyporK";
-    String endswithStr1 = name6.endswith("Cl");  // returns false
+    String endswithStr1 = name9.endswith("Cl");  // returns false
     System.out.println(endswithStr1);
-    String endswithStr2 = name6.endswith("ork"); // returns true
+    String endswithStr2 = name9.endswith("ork"); // returns true
     System.out.println(endswithStr2);
+    
+    // string indexOf
+    String name10 = "cloudyork";
+    String indexOfStr1 = name10.indexOf("ou"); 
+    System.out.println(indexOfStr1);
+    String indexOfStr2 = name10.indexOf("ork");
+    System.out.println(indexOfStr2);
+    String indexOfStr3 = name10.indexOf("o", 5);
+    System.out.println(indexOfStr3);
+    
+    // string lastIndexOf
+    String name11 = "cloudyork";
+    String lastIndexOfStr1 = name11.lastIndexOf("ou"); 
+    System.out.println(lastIndexOf1);
+    String lastIndexOfStr2 = name11.lastIndexOf("ork");
+    System.out.println(lastIndexOf2);
+    String lastIndexOfStr3 = name11.lastIndexOf("o", 5);
+    System.out.println(lastIndexOf3);
 
     
-
-    
-    
- 
-    
+    // string equals
+    String name12 = "cloudyork";
+    String equalStr1 = name12.equalsIgnoreCase("cLoudYork"); // returns true
+    System.out.println(equalStr1);
+    String equalStr2 = name12.equals("cloudyork"); // returns true
+    System.out.println(equalStr2);
+    String equalStr13 = name12.equals("oudy");  // returns false
+    System.out.println(equalStr3);
   }
   
 }
