ec8f119471f154f5c6e9851ddaa998a49bbb5968
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-08 10:06:50 +0530

    Create problem1.java

---
+++ FirstJavaProject/problem1.java
@@ -0,0 +1,10 @@
+package com.cloudy;
+import java.util.Scanner;
+public class problem1{
+  public static void main(String[] args){
+    Scanner scc = new Scanner(System.in);
+    System.out.println("Enter marks in:");
+    int subject1 = scc.nextInt();
+    
+      }
+}
