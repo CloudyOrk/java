0bfd31321a46dcc4ced8dcfb3934ac2df1ea7455
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-08 19:20:43 +0530

    Update aboutstrings.java

--- FirstJavaProject/src/com.cloudy/aboutstrings.java
+++ FirstJavaProject/src/com.cloudy/aboutstrings.java
@@ -8,7 +8,7 @@ public class aboutstrings{
     System.out.println(name);
     System.out.println(Name);
     int a = 5;
-    float b = 5.521f;
+    float b = 5.5231f;
     System.out.printf("The value of a is %d and value of b is %f and the name is %s", a, b, name); // %.2f and %8.2f
     System.out.format("The value of a is %d and value of b is %f and the name is %s");
     Scanner sc = new Scanner(System.in);
