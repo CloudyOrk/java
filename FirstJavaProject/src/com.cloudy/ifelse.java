25493e9ada1f02da8d3a53713d1ddad19d439162
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-11 17:41:17 +0530

    Create ifelse.java

---
+++ FirstJavaProject/src/com.cloudy/ifelse.java
@@ -0,0 +1,18 @@
+package com.cloudy;
+import java.util.Scanner;
+public class ifelse {
+  public static main void(String[] args) {
+    int a = 16;
+    // relational operators ==, =<, =>, !=, <, >
+    // boolean cond = (a==18);
+    // if(cond) {
+    if(a>15) {
+      System.out.println("it is greater than 15");
+    }
+    else{
+      System.out.println("it is less than 15");
+    }
+  }
+}
+
+    
