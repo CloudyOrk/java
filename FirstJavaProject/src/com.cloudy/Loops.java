b3e7a1baf79a7913c21ae571ac26fc7eb777ce0d
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-04-14 17:50:23 +0530

    Create Loops.java

---
+++ FirstJavaProject/src/com.cloudy/Loops.java
@@ -0,0 +1,29 @@
+package com.cloudy;
+import java.util.Scanner;
+public class Loops{
+  public static void main((String[] args) {
+    // while loops
+    int i = 0;
+    while(i<=1000) {
+      inc = i++
+      System.out.println(inc);
+    } 
+    System.out.println("Done!");
+
+    // infinite while loop
+    while(true) {
+      System.out.println("Infinite while loop");
+    } 
+
+    // 100 - 200 numbers 
+    int i = 100;
+    while(i<=200) {
+      inc = i++
+      System.out.println(inc);
+    } 
+    System.out.println("Done!");
+    
+  } 
+} 
+    
+    
